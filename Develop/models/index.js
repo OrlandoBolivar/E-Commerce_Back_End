f1f5932f707cf5be934239d07d6981a6c16e1f79
OrlandoBolivar <123449213+OrlandoBolivar@users.noreply.github.com>
2023-05-11 16:57:06 +1000

    index

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey:'id',
+});
 
 // Categories have many Products
+Category.hasMany(Product,{
+  foreignKey:'id',
+  OnDelete:'CASCADE',
+}),
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag,{
+  foreignKey:'id',
+  onDelete:'CASCADE',
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'id',
+  onDelete: 'CASCADE',
+});
 
 module.exports = {
   Product,
